70a5409ace96efa80eb049f6fbe1d02bfc066927
AndreyDodonov <Nordhummer@gmail.com>
2019-06-20 15:51:57 +0300

    Co-authored-by: khmelnytskiy93@rambler.ru <khmelnytskiy93@rambler.ru>

--- script.js
+++ script.js
@@ -1,42 +1,37 @@
 const searchForm = document.querySelector('#search-form');
 const movie = document.querySelector('#movies');
 
-function apiSearch(event){
+function apiSearch(event) {
     event.preventDefault(); /* что бы страница не перезагружалась */
+
     const searchText = document.querySelector('.form-control').value;
-    const server = 'https://api.themoviedb.org/3/search/multi?api_key=537a6d92902c73e213db4ccffd38483b&language=ru-RU&query=' + searchText ;
-    requestApi('GET', server);
-}
+    const server = 'https://api.themoviedb.org/3/search/multi?api_key=537a6d92902c73e213db4ccffd38483b&language=ru-RU&query=' + searchText;
+    movie.innerHTML = 'загрузка';
 
-searchForm.addEventListener('submit', apiSearch);
-
-function requestApi(method, url){
-    const request = new XMLHttpRequest();
-    /*console.log(request); */
-    request.open(method, url);   
-    request.send();
-    request.addEventListener('readystatechange', () => {      /* стрелочная функция */
-        if (request.readyState !== 4) return;
-        if (request.status !== 200) {
-            console.log('error: '+ request.status);
-            
-            return;
-        }
-        const output = JSON.parse(request.responseText);  
-
-        let inner = '';
-
-        output.results.forEach(function (item){
-            let nameItem = item.name || item.title;
-            let dateItem = item.release_date;
-            inner += `<div class="col-12">${nameItem}<br><b>Дата выхода: ${dateItem}</b></div>`;   /* '<div class="col-12">'+ nameItem + '</div>' */
+    fetch(server)
+        .then(function(value){
+            if (value.status !== 200) {
+                return Promise.reject(value);
+            }
+            return value.json();
+        })
+        .then(function(output){
+            let inner = '';
+            output.results.forEach(function (item) {
+                
+                let nameItem = item.name || item.title;
+                let dateItem = item.release_date;
+                inner += `
+                    <div class="col-3 item">
+                    <b>Название: ${nameItem}<br>
+                    Дата выхода: ${dateItem}</b></div>
+                `;
+            });
+            movie.innerHTML = inner;
+        })
+        .catch(function(reason) {
+            movie.innerHTML = 'Упс, что-то пошло не так!';
+            console.log('error: ' + reason.status );
         });
-
-        movie.innerHTML = inner;  /* отправляем на страницу */
-        /*console.log(output); */
-        /* console.log(request); */
-        /* console.log(request.responseText); */ 
-        /* console.log(request.readyState); */
-
-    });
-}
+    }
+searchForm.addEventListener('submit', apiSearch);   
